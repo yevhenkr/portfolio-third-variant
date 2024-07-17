1d2256902df350351ff4355a438844c0de631f48
Eugen <vasilenkoevgen88@gmail.com>
2024-07-17 16:31:13 +0300

    feat: section maine

--- src/common/components/layout/main/main.tsx
+++ src/common/components/layout/main/main.tsx
@@ -1,17 +1,26 @@
 import {UserSVG} from "../../../../assets/icons/user";
+import styled from "styled-components";
+import {FlexWrapper} from "../../ui/flexWrapper/FlexWrapper";
+import userPhoto from "../../../../assets/img/userPhoto.jpg";
 
 export const Main = () => {
     return (
-        <main>
-            <div>
-                <span>Hi !</span>
-                <h2>I’m Muhamad Raul.</h2>
-                <h1>a Front-End Developer</h1>
-                <p>Front-End devel oper based in Bekasi, Indonesian
-                    I’am coding with a clean and beautiful problem
-                    solving in mind.</p>
-            </div>
-            <UserSVG/>
-        </main>
+        <StyledMain>
+            <FlexWrapper display={"flex"} flexDirection={"column"}>
+                <StyledH1>a Front-End Developer</StyledH1>
+                <StyledH2>I’m Muhamad Raul.</StyledH2>
+            </FlexWrapper>
+            <StyledIcon src={userPhoto} alt={"User photo"}></StyledIcon>
+        </StyledMain>
     );
 };
+const StyledMain = styled.main`
+    display: flex;
+    flex-direction: row;
+`
+const StyledH1 = styled.h1``
+const StyledH2 = styled.h2``
+const StyledIcon = styled.img`
+max-width: 350px;
+width: 100%;
+`
